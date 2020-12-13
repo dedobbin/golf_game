aa1c51a4afe302e1c49abca10da3a7d30e5ef243
debobbin <you@example.com>
2020-12-13 23:06:48 +0100

    added animation class wip

--- sprite.hpp
+++ sprite.hpp
@@ -3,6 +3,9 @@
 
 #include <SDL2/SDL.h>
 #include "camera.hpp"
+#include <map>
+#include <memory>
+#include "animation.hpp"
 // #include <SDL2/SDL_image.h>
 
 // circ dep
@@ -15,6 +18,8 @@ class Sprite
 		~Sprite();
 		SDL_Rect getPos(Camera* camera = NULL);
 		Entity* owner;
+		int activeAnimation = 0;
+		std::map<int, std::unique_ptr<Animation>> animations;
 };
 
 #endif
