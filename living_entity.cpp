91b76a1ff399e8dcdf2f4896d5369781e8e7f230
debobbin <you@example.com>
2021-01-14 21:30:12 +0100

    drop item wip

--- living_entity.cpp
+++ living_entity.cpp
@@ -1,4 +1,5 @@
 #include "living_entity.hpp"
+#include "direction.hpp"
 #include <iostream>
 
 LivingEntity::LivingEntity(std::string name, int x, int y, int w, int h)
@@ -14,6 +15,11 @@ void LivingEntity::give(Item* item)
 
 void LivingEntity::dropHeldItem()
 {
+	direction throwDir = this->behavior->xSpeed > 0 ? RIGHT : LEFT;
+	this->heldItem->pos.x = throwDir == RIGHT ? this->pos.x + this->pos.w + 10 : this->pos.x - 10;
+	this->heldItem->behavior->xSpeed = RIGHT ? 4 : -4;
+	this->heldItem->behavior->ySpeed = this->behavior->ySpeed;
+
 	this->heldItem->owner = NULL;
 	this->heldItem = NULL;
 }
