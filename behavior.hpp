2bb29ff4a09fce055e2bf4a010af8945eb4d6aae
debobbin <you@example.com>
2021-01-17 00:34:45 +0100

    updated comments

--- behavior.hpp
+++ behavior.hpp
@@ -21,8 +21,8 @@ class Behavior
 		/* When slowing down, clampZero should be true so rounds to 0 eventually */
 		void addYSpeed(float n, bool clampZero = false);
 		float walkAcc = 0.2;
-		float xSpeed = 0; //should be set through addXSpeed() 
-		float ySpeed = 0; //should be set through addXSpeed() 
+		float xSpeed = 0; //should be set through addXSpeed(), would be better to properly private but eh
+		float ySpeed = 0; //should be set through addXSpeed(), would be better to properly private but eh 
 		float maxXSpeed = 7.0;
 		float maxYSpeed = 20.0;
 		bool gravity = true;
--- living_entity.hpp
+++ living_entity.hpp
@@ -10,6 +10,7 @@ class LivingEntity : public Entity
 		LivingEntity(std::string name, int x = 0, int y = 0, int w = 100, int h = 100);
 		~LivingEntity();
 		void give(Item* item);
+		/* Should be set using give, so owner of item is also set correctly, would be better to properly private but eh */
 		Item* heldItem = NULL;
 		
 };
