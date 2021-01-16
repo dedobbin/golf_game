84ca7f6735beeee2d61b0157b9f6c0b361b8d523
debobbin <you@example.com>
2021-01-17 00:30:33 +0100

    remove unused second constructor for item

--- item.cpp
+++ item.cpp
@@ -1,10 +1,6 @@
 #include "item.hpp"
 #include <iostream>
 
-Item::Item(std::string name, int w, int h, Entity* owner)
-: Entity(name, ITEM, owner->pos.x, owner->pos.x, w, h)
-{}
-
 Item::Item(std::string name, int x, int y, int w, int h)
 : Entity(name, ITEM, x, y, w, h)
 {}
--- item.hpp
+++ item.hpp
@@ -7,7 +7,6 @@
 class Item : public Entity
 {
 	public:
-		Item(std::string name, int w, int h, Entity* Owner);
 		Item(std::string name, int x, int y, int w, int h);
 		~Item();
 		Entity* owner = NULL;
