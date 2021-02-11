f367b6f7a06e41425a502c84c5b14f7f18cfcfb7
debobbin <you@example.com>
2021-02-11 20:35:27 +0100

    player in front of enemy

--- main.cpp
+++ main.cpp
@@ -36,10 +36,10 @@ void generateEntities(std::unordered_map<std::string, SDL_Texture*> spritesheets
 
 	World::entities.emplace_back(factory.createGolfClub(300, 0));
 
+	World::entities.emplace_back(factory.createEnemy(300, 0));
+
 	World::entities.emplace_back(factory.createPlayer(100, 0));
 	player = std::static_pointer_cast<LivingEntity>(World::entities.back());
-
-	World::entities.emplace_back(factory.createEnemy(300, 0));
 
 	World::entities.emplace_back(factory.createBall(100, 0));
 
